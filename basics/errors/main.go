cc512f4ead6823b75b5fb1cbea16d5e4b2ff16bf
Eloy Lopez Penamaria <elopez@trileucosolutions.com>
2019-11-30 00:11:47 +0100

    Add errors propagation example

---
+++ basics/errors/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+)
+
+type Rocket struct {
+	fuel        int
+	engine      bool
+	temperature int
+}
+
+func NewRocket(fuel int, temperature int) *Rocket {
+	return &Rocket{fuel: fuel, temperature: temperature}
+}
+
+func (r Rocket) Start() error {
+	if err := r.checkTemperature(); err != nil {
+		fmt.Println(err)
+	}
+	if err := r.initEngine(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r Rocket) checkTemperature() error {
+	const MaxTemperature = 100
+	if r.temperature > MaxTemperature {
+		msg := "temperature warning ! %v is higher than the limit %v !"
+		return errors.New(fmt.Sprintf(msg, r.temperature, MaxTemperature))
+	}
+	return nil
+}
+
+func (r Rocket) initEngine() error {
+	if r.fuel < 1 {
+		return errors.New("Cannot start without fuel")
+	}
+	r.engine = true
+	return nil
+}
+
+func main() {
+
+	fmt.Println("---> Rocket 1 is starting !!")
+	r1 := NewRocket(10, 50)
+	if err := r1.Start(); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("---> Rocket 1 started.")
+
+	fmt.Println("---> Rocket 2 is starting !!")
+	r2 := NewRocket(10, 150)
+	if err := r2.Start(); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("---> Rocket 2 started !!")
+
+	fmt.Println("---> Rocket 3 is starting !!")
+	r3 := NewRocket(0, 10)
+	if err := r3.Start(); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("---> Rocket 3 started !!")
+}
