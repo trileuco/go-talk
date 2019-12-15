9d7e7bd62d92544669bfda82c0ec4289698be0eb
Eloy Lopez Penamaria <elopez@trileucosolutions.com>
2019-12-15 15:00:37 +0100

    Add zero value example

---
+++ basics/2_zero_value/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var active bool
+	var amount int
+	var name string
+	var price float64
+
+	fmt.Println(active) // false
+	fmt.Println(amount) // 0
+	fmt.Println(name)   // ""
+	fmt.Println(price)  // 0
+
+}
